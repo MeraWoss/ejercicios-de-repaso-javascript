42a853c843d99744b68dc2864eaf52f925ef7991
MeraWoss <92162315+MeraWoss@users.noreply.github.com>
2022-02-18 14:10:55 +0100

    Add files via upload

---
+++ ejercicios de repaso/style/2.js
@@ -0,0 +1,9 @@
+function sacaredad() {
+    var edad = document.getElementById("campotexto").value;
+    if (edad >= 18){
+        alert('eres mayor de edad');
+    }
+    else {
+        alert ('eres menor de edad');
+    }
+}
